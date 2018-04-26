813a3bd6ccdc9d1be1530234928831c1971576ac
Alain Chiasson <alain.chiasson@guavus.com>
2018-04-26 08:55:41 -0400

    Created App the prints out IP and path

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+import socket
+from flask import Flask,request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello! I am a Flask application'
+    return 'Hello! I am a Flask application.'
+
+@app.route('/env')
+def print_env():
+    return socket.gethostbyname(socket.gethostname())
+
+@app.route('/<path:full_path>')
+def print_path(full_path):
+    ip = socket.gethostbyname(socket.gethostname())
+    return 'Hello! I am a Flask application on %s. Request path: %s' % (ip, full_path)
 
 if __name__ == '__main__':
     # Note the extra host argument. If we didn't have it, our Flask app
