dc253b66899ac543a56239cfbbd00e23e63048e4
Alain Chiasson <alain.chiasson@guavus.com>
2018-05-03 08:40:27 -0400

    Added full header printing

--- app.py
+++ app.py
@@ -9,16 +9,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '%s : Hello! I am a Flask application.' % tag
+    return '%s : Hello! I am a Flask application.' % tag + get_raw_headers(request)
+
+
+@app.route('/request')
+def print_request():
+    return get_raw_headers(request)
 
 @app.route('/env')
 def print_env():
-    return '%s : %s' % (tag, socket.gethostbyname(socket.gethostname()))
+    return '%s : %s' % (tag, socket.gethostbyname(socket.gethostname())) + get_raw_headers(request)
 
 @app.route('/<path:full_path>')
 def print_path(full_path):
     ip = socket.gethostbyname(socket.gethostname())
-    return '%s : Hello! I am a Flask application on %s. Request path: %s' % (tag, ip, full_path)
+    return '%s : Hello! I am a Flask application on %s. Request path: %s' % (tag, ip, full_path) + get_raw_headers(request)
+
+def get_raw_headers(request):
+    headerlist = ""
+    for key, value in request.headers.iteritems():
+        headerlist = headerlist + "\n%s: %s" % (key, value)
+    return headerlist
+
 
 if __name__ == '__main__':
     # Note the extra host argument. If we didn't have it, our Flask app
